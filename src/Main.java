e4745daa7066d989194835797c2bbe0d7ace736d
DanaSnellens <danasnellens@gmail.com>
2024-03-21 18:24:37 +0100

    Arrays (int en string) van nummers aangemaakt & class Translator aangemaakt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
+        int[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
 
     }
